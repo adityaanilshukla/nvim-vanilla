25ef263ea843623dbcb50575889096625525eb79
adityaanilshukla <adityaanilsindhunath@gmail.com>
2024-02-16 21:56:46 +0800

    add find word and colorschemes using telescope

--- lua/aditya/plugins/telescope.lua
+++ lua/aditya/plugins/telescope.lua
@@ -4,3 +4,22 @@ local builtin = require('telescope.builtin')
 -- telescope command
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Keybinding for Telescope Colorscheme Picker
+vim.keymap.set('n', '<leader>ft', function()
+    require('telescope.builtin').colorscheme({ enable_preview = true })
+end, { desc = 'Find themes' })
+
+-- Keybinding for Telescope Live Grep (Search for words)
+vim.keymap.set('n', '<leader>fw', function()
+    require('telescope.builtin').live_grep()
+end, { desc = 'Find words' })
+
+-- Keybinding for Telescope Live Grep including hidden files and ignoring .gitignore
+vim.keymap.set('n', '<leader>fW', function()
+    require('telescope.builtin').live_grep({
+        additional_args = function(args)
+            return vim.tbl_extend('force', args, { '--hidden', '--no-ignore' })
+        end
+    })
+end, { desc = 'Find words in all files' })
