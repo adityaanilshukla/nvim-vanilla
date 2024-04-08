54340f7d04e800c39f61cd4897d85e5b38279aa3
adityaanilshukla <adityaanilsindhunath@gmail.com>
2024-04-08 08:04:39 +0800

    make control + 7 toggle the terminal rather than 7

--- lua/aditya/plugins/toggleterm.lua
+++ lua/aditya/plugins/toggleterm.lua
@@ -21,12 +21,10 @@ _G.toggle_floating_terminal = function()
 end
 
 
--- Toggle the general floating terminal with F7 and 7 in normal mode
+-- Toggle the general floating terminal with F7 and control + 7 in normal mode
 vim.api.nvim_set_keymap('n', '<F7>', '<cmd>lua toggle_floating_terminal()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '7', '<cmd>lua toggle_floating_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-7>', '<cmd>lua toggle_floating_terminal()<CR>', { noremap = true, silent = true })
 
-
-
--- Toggle the general floating terminal with F7 and 7 in terminal mode
+-- Toggle the general floating terminal with F7 and control + 7 in terminal mode
 vim.api.nvim_set_keymap('t', '<F7>', '<C-\\><C-n><cmd>lua toggle_floating_terminal()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '7', '<C-\\><C-n><cmd>lua toggle_floating_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-7>', '<C-\\><C-n><cmd>lua toggle_floating_terminal()<CR>', { noremap = true, silent = true })
