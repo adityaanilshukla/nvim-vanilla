343bf5698e336523659cb3e2e13fa3588eb015cb
adityaanilshukla <adityaanilsindhunath@gmail.com>
2024-07-23 02:45:23 +0800

    clean up code

--- lua/aditya/core/options.lua
+++ lua/aditya/core/options.lua
@@ -24,14 +24,6 @@ require('session_manager').setup({
   autoload_mode = config.AutoloadMode.Disabled, -- Disable autoload
 })
 
--- Then, set up an autocommand to open Alpha when Neovim starts
--- vim.api.nvim_create_autocmd("VimEnter", {
---     pattern = "*",
---     callback = function()
---         require("alpha").start()
---     end,
--- })
-
 require("telescope").setup {
   extensions = {
     ["ui-select"] = {
--- lua/aditya/plugins/vimtex.lua
+++ lua/aditya/plugins/vimtex.lua
@@ -9,15 +9,5 @@ vim.g.vimtex_quickfix_mode = 0
 vim.g.vimtex_syntax_enabled = 1
 vim.g.vimtex_fold_enabled = 1
 
--- Define custom mappings if needed
-vim.cmd [[
-  augroup vimtex_event_handlers
-    autocmd!
-    autocmd FileType tex nmap <buffer> <leader>lv :VimtexView<CR>
-    autocmd FileType tex nmap <buffer> <leader>lc :VimtexCompile<CR>
-    autocmd FileType tex nmap <buffer> <leader>le :VimtexErrors<CR>
-  augroup END
-]]
-
 -- VimTeX compile 
 vim.api.nvim_set_keymap('n', '<leader>v', ':VimtexCompile<CR>', { noremap = true, silent = true, desc = 'Vimtex Compile Doc' })
